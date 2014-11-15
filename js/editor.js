7e3ab3b740df17496924137b2985cd5930dbd44e
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-15 18:01:14 +0800

    update json editor

--- js/editor.js
+++ js/editor.js
@@ -1,12 +1,40 @@
 (function(){
-  
-   var editor = ace.edit("editor");
-  
-   editor.getSession().setMode("ace/mode/json");
 
-   editor.getSession().on('change', function(e) {
+   module.exports = getEditor;
 
-   });
+   function getEditor() {
 
+     var editor = ace.edit("editor");
+      
+     editor.init = init;
+     editor.watch = watch;
+     editor.parseJson = parseJson;
+     editor.getEditor = getEditor;
+     editor.init();
 
+     return editor;
+
+     function init(){
+        editor.getSession().setMode("ace/mode/json");
+        editor.getSession().on('change', watch);
+     }            
+
+     function watch() {
+       var raw_json = parseJson(editor.getValue());
+       if(raw_json) {
+       }
+     }
+
+     function parseJson(){
+       try {
+          return JSON.parse(raw_text);
+       } catch (e) {
+          return null;
+       }
+     }
+
+     function getEditor() {
+        return editor;   
+     }
+   }
 })();
---
+++ js/json_ui.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+
+  var jsonEditor = require("../js/editor")();
+
+  angular.module('json.ui', ['ngRoute']);
+
+
+})();
