3dcbbd17b23a8530fc3da8c1d5943b096a73b556
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-17 01:08:50 +0800

    update form controller

--- js/controllers/form.controller.js
+++ js/controllers/form.controller.js
@@ -1,35 +1,60 @@
 (function(){
   'use strict';
 
-  module.exports = require('angular')
-                   .module('json.ui')
-                   .controller('FormController', formController);
+  module.exports = formController;
 
-  formController.$inject = ['$scope', '$element'];
+  function formController($scope, $element, $compile, objectService) {
 
-  function formController($scope, $element) {
-    
-    this.addValue = addValue;
+    var jsonValue = '<input type="text"></input>';
+    var jsonChildObject = null;
+    var jsonSiblingObject = null;
+
     this.addObject = addObject;
-    this.addArray = addArray;
     this.updateInput = updateInput;
 
-    function addValue(){
-      var value = "<input type='text'></input>";
-      $element.append(value);
-    }
+    getObject();
 
-    function addObject(){
-    
-    }
-
-    function addArray(){
-    
+    function getObject(){
+      objectService.getObject()
+        .success(function (data) {
+          jsonSiblingObject = '<object class="sibling">' + data + '</object>';
+          jsonChildObject =  '<object class="child">' + data + '</object>';
+        })
+        .error(function (error) {
+          console.log(error);
+        });
     }
 
     function updateInput() {
-    } 
+    }
 
+    window.replace = function (sel) {
+
+      var selectType = sel.value;
+      var thisDOM = angular.element(sel);
+      var scope = angular.element(sel).scope();
+
+      if(selectType === 'value') {
+        thisDOM.after(jsonValue);
+      }
+      else if(selectType === 'array') {
+      }
+      else if(selectType === 'object') {
+        var el = $compile('<div class="json-wrap">'
+                          + jsonSiblingObject + '</div>')( $scope );
+        thisDOM.after(el);
+      }
+      thisDOM.remove();
+    }
+
+    function addObject($event) {
+      var thisObject = angular.element($event.srcElement).parent();
+      var el = $compile(jsonSiblingObject)( $scope );
+      thisObject.after(el);
+    }
+
+    function updateInput() {
+    }
   }
 
 })();
