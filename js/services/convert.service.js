dcd8fc36edfca6c0b804b5c0b8a6fefaee1dcc6d
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-19 00:53:56 +0800

    re string as variable

--- js/services/convert.service.js
+++ js/services/convert.service.js
@@ -13,17 +13,22 @@
 
     function toDocument(rawJson, json) {
 
-      var keyPrefix = "<key><input type='text' value='" 
+      var keyPrefix = "<key><input type='text' value='" ;
       var keySuffix = "'></input></key><span>--</span>";
-      var removeButton =  "<button class='remove' onclick='removeSelf(this)'>-</button>";
-      var addObjButton = "<button class='add' onclick='addObject(this)'>+</button>" 
-      var appendEleButton = '<button class="add" onclick="appendElement(this)">+</button>'
-
-      var valuePrefix = "<value><input type='text' value='"
+      var valuePrefix = "<value><input type='text' value='";
       var valueSuffix = "'></input></value>";
+
+      var removeButton =  "<button class='remove' onclick='removeSelf(this)'>-</button>";
+      var addObjButton = "<button class='add' onclick='addObject(this)'>+</button>" ;
+      var appendEleButton = '<button class="add" onclick="appendElement(this)">+</button>';
 
       var keyReg = /"([^,:{}\[\]]*)"\s*:/g;
       var valueReg = /"([^,:{}\[\]]*)"/g;
+      var objPrefixReg = /{/g;
+      var objSuffixReg = /}/g;
+      var arrayPrefixReg = /\[/g;
+      var arraySuffixReg = /\]/g;
+      var comma = /,/g;
 
       rawJson = rawJson.replace(keyReg, '<key-value>' + removeButton + addObjButton 
                                 + keyPrefix + '$1' + keySuffix );
@@ -31,12 +36,12 @@
       rawJson = rawJson.replace(valueReg, valuePrefix 
                                 +'$1' + valueSuffix);
 
-      rawJson = rawJson.replace(/,\s*<key-value>/g , '</key-value><key-value>');
-      rawJson = rawJson.replace(/{/g, "<object class='child'>");
-      rawJson = rawJson.replace(/}/g, '</object>');
-      rawJson = rawJson.replace(/\[/g, '<array><element>');
-      rawJson = rawJson.replace(/\]/g,  removeButton + appendEleButton + '</element></array>');
-      rawJson = rawJson.replace(/[,]/g, '');
+      rawJson = rawJson.replace(/,\s*<key-value>/g, '</key-value><key-value>');
+      rawJson = rawJson.replace(objPrefixReg, "<object class='child'>");
+      rawJson = rawJson.replace(objSuffixReg, '</object>');
+      rawJson = rawJson.replace(arrayPrefixReg, '<array><element>');
+      rawJson = rawJson.replace(arraySuffixReg,  removeButton + appendEleButton + '</element></array>');
+      rawJson = rawJson.replace(comma, '');
 
       return rawJson.substring(22, rawJson.length-10);;
     }
