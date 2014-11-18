dab9b1e89acb8902a304137a5e10aa852c14289a
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-18 11:40:57 +0800

    add jsonUIController

--- js/json_ui.js
+++ js/json_ui.js
@@ -9,8 +9,15 @@ require('angular-route');
 
 var app = angular.module('json.ui', ['ngRoute']);
 
-require("../js/services/object.services");
+require("../js/services/object.service");
+require("../js/services/value.service");
+require("../js/services/array.service");
+require("../js/services/parser.service");
 require("../js/directives/object.directive");
+require("../js/directives/value.directive");
+require("../js/directives/array.directive");
+var jsonUIController = require("../js/controllers/jsonUI.controller");
+app.controller('jsonUIController', jsonUIController);
 
 function watch() {
   var raw_json = jsonEditor.getJson();
