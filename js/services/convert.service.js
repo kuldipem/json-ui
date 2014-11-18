d1cc73edb74a92d2ae305f5c6a49bb9fd5534575
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-19 00:09:06 +0800

    add element

--- js/services/convert.service.js
+++ js/services/convert.service.js
@@ -12,8 +12,9 @@
 
       var keyPrefix = "<key><input type='text' value='" 
       var keySuffix = "'></input></key><span>--</span>";
-      var buttons =  "<button class='remove' onclick='removeSelf(this)'>-</button>"
-                   + "<button class='add' onclick='addObject(this)'>+</button>" 
+      var removeButton =  "<button class='remove' onclick='removeSelf(this)'>-</button>";
+      var addObjButton = "<button class='add' onclick='addObject(this)'>+</button>" 
+      var appendEleButton = '<button class="add" onclick="appendElement(this)">+</button>'
 
       var valuePrefix = "<value><input type='text' value='"
       var valueSuffix = "'></input></value>";
@@ -21,20 +22,18 @@
       var keyReg = /"([^,:{}\[\]]*)"\s*:/g;
       var valueReg = /"([^,:{}\[\]]*)"/g;
 
-      rawJson = rawJson.replace(keyReg, '<key-value>' + buttons + 
-                                keyPrefix + '$1' + keySuffix );
+      rawJson = rawJson.replace(keyReg, '<key-value>' + removeButton + addObjButton 
+                                + keyPrefix + '$1' + keySuffix );
 
-      rawJson = rawJson.replace(valueReg, buttons + valuePrefix +
-                                '$1' + valueSuffix);
+      rawJson = rawJson.replace(valueReg, valuePrefix 
+                                +'$1' + valueSuffix);
 
-      //rawJson = rawJson.replace(/[\[](.*),(.*)[\]]/g, '[$1' + ' ' + '$2]');
-      //rawJson = rawJson.replace(/,/g , '</key-value>');
+      rawJson = rawJson.replace(/,\s*<key-value>/g , '</key-value><key-value>');
       rawJson = rawJson.replace(/{/g, "<object class='child'>");
       rawJson = rawJson.replace(/}/g, '</object>');
-      rawJson = rawJson.replace(/\[/g, '<array>');
-      rawJson = rawJson.replace(/\]/g, '</array>');
+      rawJson = rawJson.replace(/\[/g, '<array><element>');
+      rawJson = rawJson.replace(/\]/g,  removeButton + appendEleButton + '</element></array>');
       rawJson = rawJson.replace(/[,]/g, '');
-
 
       return rawJson.substring(22, rawJson.length-10);;
     }
