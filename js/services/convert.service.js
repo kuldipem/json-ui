c9cfad0860f6a7b8b5698abc0bec822f84dbd663
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-18 23:02:43 +0800

    update conver.service.js

--- js/services/convert.service.js
+++ js/services/convert.service.js
@@ -9,7 +9,6 @@
     this.toDocument = toDocument;
 
     function toDocument(rawJson, json) {
-      console.log(rawJson);
 
       var keyPrefix = "<key><input type='text' value='" 
       var keySuffix = "'></input></key><span>--</span>";
@@ -19,23 +18,25 @@
       var valuePrefix = "<value><input type='text' value='"
       var valueSuffix = "'></input></value>";
 
-      var keyReg = /"([^:{}]*)"\s*:/g;
-      var valueReg = /"(.*)"/g;
+      var keyReg = /"([^,:{}\[\]]*)"\s*:/g;
+      var valueReg = /"([^,:{}\[\]]*)"/g;
 
-      rawJson = rawJson.replace(/[,|\s]/g, '');
-      rawJson = rawJson.replace(keyReg, '<key-value>'+ buttons + 
+      rawJson = rawJson.replace(keyReg, '<key-value>' + buttons + 
                                 keyPrefix + '$1' + keySuffix );
 
       rawJson = rawJson.replace(valueReg, valuePrefix + '$1' +
-                            valueSuffix + '</key-value>');
+                            valueSuffix);
 
+      //rawJson = rawJson.replace(/[\[](.*),(.*)[\]]/g, '[$1' + ' ' + '$2]');
+      //rawJson = rawJson.replace(/,/g , '</key-value>');
       rawJson = rawJson.replace(/{/g, "<object class='child'>");
       rawJson = rawJson.replace(/}/g, '</object>');
       rawJson = rawJson.replace(/\[/g, '<array>');
       rawJson = rawJson.replace(/\]/g, '</array>');
+      rawJson = rawJson.replace(/[,]/g, '');
 
 
-      return rawJson.substring(22, rawJson.length-9);;
+      return rawJson.substring(22, rawJson.length-10);;
     }
 
   }
